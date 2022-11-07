3a478ae530df69b678d8b5c6e29b1af96854b079
Andrew Talanov <andrewtalanov@mail.ru>
2022-11-07 16:10:12 +0500

    поставил скрин сайтов по центру

--- src/components/portfolio-item/PortfolioItem.js
+++ src/components/portfolio-item/PortfolioItem.js
@@ -3,9 +3,7 @@ import styles from "./PortfolioItem.module.scss";
 
 const PortfolioItem = ({img, flag, title, id}) => {
 
-        // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
     let vh = window.innerHeight * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty('--vh', `${vh}px`);
 
     (function init100vh(){
